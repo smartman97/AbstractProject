cd43b86f109a3121e470787b77916bda6dbd4983
Colm Laro <smartman97@gmail.com>
2016-04-01 09:58:05 -0600

    ArrayList of object added.

--- src/myAbstract/controller/AbstractController.java
+++ src/myAbstract/controller/AbstractController.java
@@ -2,19 +2,36 @@ package myAbstract.controller;
 
 import java.util.ArrayList;
 
+import myAbstract.model.BicycleDardDeck;
+import myAbstract.model.CallofDuty;
 import myAbstract.model.InternetThings;
+import myAbstract.model.MountainDew;
 
 public class AbstractController
 {
 	private ArrayList<InternetThings> InternetThings; 
+	private CallofDuty cod;
+	private BicycleDardDeck card;
+	private MountainDew dew;
 	
 	public AbstractController()
 	{
+		InternetThings = new ArrayList<InternetThings>();
 		
+		cod = new CallofDuty();
+		card = new BicycleDardDeck();
+		dew = new MountainDew();
+		
+		InternetThings.add(cod);
+		InternetThings.add(card);
+		InternetThings.add(dew);
 	}
 	
 	public void start()
 	{
-		
+		for(int i = 0; i < InternetThings.size(); i++)
+		{
+			InternetThings.get(i).troll(2);
+		}
 	}
 }
--- src/myAbstract/controller/AbstractRunner.java
+++ src/myAbstract/controller/AbstractRunner.java
@@ -4,6 +4,8 @@ import myAbstract.controller.*;
 
 public class AbstractRunner
 {
+	public AbstractController baseController;
+	
 	public static void main(String[] args)
 	{
 		AbstractController baseController = new AbstractController();
